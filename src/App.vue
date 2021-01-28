89c721b9ba535a2234ae822e1b6ed4757517cb42
changmingyuan <1025230505@qq.com>
2021-01-28 17:34:00 +0800

    日常

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
     <h2>hahaha</h2>
+    <router-view />
   </div>
 </template>
 
 <script>
+import Home from 'views/Home'
 
-
-  export default {
-    name: 'app',
-    components: {
-
-    }
-  }
+export default {
+  name: "app",
+  components: {
+    Home
+  },
+};
 </script>
 
 <style>
-  @import "`assets/style/base.css";
+@import "`assets/style/base.css";
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>我是首页哈哈哈</h2>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'Home',
+    props:[''],
+    data () {
+      return {
+
+      };
+    },
+
+    components: {},
+
+    computed: {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    methods: {},
+
+    watch: {}
+
+  }
+
+</script>
+<style lang='' scoped>
+
+</style>
